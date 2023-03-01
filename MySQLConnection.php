016a074e41e610811b69befd77637c0fa4b26f04
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-01 23:34:23 +0200

    Add files via upload

---
+++ MySQLConnection.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Db;
+
+use PDO;
+use Db\UniversalConfig;
+
+class MySQLConnection implements ConnectionInterface {
+
+    private $config;
+    private $conn;
+    private $resource;
+    public $connected;
+    public $error;
+    public $errno;
+    public $connect_error;
+    public $connect_errno;
+    public $affected_rows = 0;
+    public $insert_id = 0;
+
+    public function __construct($config = null) {
+        if ($config) {
+            $this->config = $config;
+        } else {
+            $this->config = new UniversalConfig();
+        }
+    }
+
+    public function connect($config = null) {
+        if ($config) {
+            $this->config = $config;
+        }
+        try {
+            $this->conn = new PDO(
+                    "{$this->config->getDriver()}:" .
+                    (
+                    $this->config->hasUnixSocket() ?
+                    "unix_socket={$this->config->getUnixSocket()};" :
+                    "host={$this->config->getHost()};" . "port={$this->config->getPort()};"
+                    ) .
+                    "dbname={$this->config->getDbname()};" .
+                    "charset={$this->config->getCharset()}",
+                    $this->config->getUsername(),
+                    $this->config->getPassword(),
+                    $this->config->getOptions()
+            );
+            $this->connected = true;
+        } catch (PDOException $ex) {
+            $this->connect_error = $this->conn->connect_error;
+            $this->connected = false;
+        }
+        return $this->connected;
+    }
+
+    public function begin() {
+        if (!$this->connected) {
+            $this->connect($this->config);
+        }
+        $this->affected_rows = 0;
+        $this->insert_id = 0;
+        try {
+            $this->conn->beginTransaction();
+        } catch (PDOException $ex) {
+            $this->error = $ex->getMessage();
+            $this->errno = $ex->getCode();
+            return false;
+        }
+        $this->error = '';
+        $this->errno = null;
+        return $this;
+    }
+
+    public function commit() {
+        if (!$this->connected) {
+            $this->connect($this->config);
+        }
+        $this->affected_rows = 0;
+        $this->insert_id = 0;
+        try {
+            $this->conn->commit();
+        } catch (PDOException $ex) {
+            $this->error = $ex->getMessage();
+            $this->errno = $ex->getCode();
+            return false;
+        }
+        $this->error = '';
+        $this->errno = null;
+        return $this;
+    }
+
+    public function query($sql) {
+        if (!$this->connected) {
+            $this->connect($this->config);
+        }
+        try {
+            $this->resource = $this->conn->query($sql);
+        } catch (PDOException $ex) {
+            $this->error = $ex->getMessage();
+            $this->errno = $ex->getCode();
+            return false;
+        }
+        $this->error = '';
+        $this->errno = null;
+        $this->affected_rows = $this->resource->rowCount();
+        $this->insert_id = $this->resource->lastInsertId();
+        return $this;
+    }
+
+    public function fetch($mode = PDO::FETCH_DEFAULT) {
+        if (!$this->resource) {
+            return false;
+        }
+        return $this->resource->fetch($mode);
+    }
+
+    public function fetchAll($mode = PDO::FETCH_DEFAULT) {
+        if (!$this->resource) {
+            return false;
+        }
+        return $this->resource->fetchAll($mode);
+    }
+
+    public function lastInsertId() {
+        return $this->insert_id;
+    }
+
+    public function rowCount() {
+        return $this->affected_rows;
+    }
+
+}
---
+++ UniversalConfig.php
@@ -0,0 +1,176 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Db;
+
+class UniversalConfig {
+
+    public const DRIVER_MYSQL = 'mysql';
+    public const DRIVER_PGSQL = 'pgsql';
+    public const DRIVER_REDIS = 'redis';
+
+    protected $driver = self::DRIVER_MYSQL;
+    protected $host = '127.0.0.1';
+    protected $port = 3306;
+    protected $unixSocket;
+    protected $dbname = 'test';
+    protected $charset = 'utf8mb4';
+    protected $username = 'root';
+    protected $password = 'root';
+    protected $options = [];
+    protected $timeout = 0.0;
+    protected $reserved = '';
+    protected $retry_interval = 0;
+    protected $read_timeout = 0.0;
+    protected $auth = '';
+    protected $dbIndex = 0;
+
+    public function getDriver(): string {
+        return $this->driver;
+    }
+
+    public function withDriver(string $driver): self {
+        $this->driver = $driver;
+        return $this;
+    }
+
+    public function getHost(): string {
+        return $this->host;
+    }
+
+    public function withHost($host): self {
+        $this->host = $host;
+        return $this;
+    }
+
+    public function getPort(): int {
+        return $this->port;
+    }
+
+    public function hasUnixSocket(): bool {
+        return isset($this->unixSocket);
+    }
+
+    public function getUnixSocket(): string {
+        return $this->unixSocket;
+    }
+
+    public function withUnixSocket(?string $unixSocket): self {
+        $this->unixSocket = $unixSocket;
+        return $this;
+    }
+
+    public function withPort(int $port): self {
+        $this->port = $port;
+        return $this;
+    }
+
+    public function getDbname(): string {
+        return $this->dbname;
+    }
+
+    public function withDbname(string $dbname): self {
+        $this->dbname = $dbname;
+        return $this;
+    }
+
+    public function getCharset(): string {
+        return $this->charset;
+    }
+
+    public function withCharset(string $charset): self {
+        $this->charset = $charset;
+        return $this;
+    }
+
+    public function getUsername(): string {
+        return $this->username;
+    }
+
+    public function withUsername(string $username): self {
+        $this->username = $username;
+        return $this;
+    }
+
+    public function getPassword(): string {
+        return $this->password;
+    }
+
+    public function withPassword(string $password): self {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getOptions(): array {
+        return $this->options;
+    }
+
+    public function withOptions(array $options): self {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function getTimeout(): float {
+        return $this->timeout;
+    }
+
+    public function withTimeout(float $timeout): self {
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    public function getReserved(): string {
+        return $this->reserved;
+    }
+
+    public function withReserved(string $reserved): self {
+        $this->reserved = $reserved;
+        return $this;
+    }
+
+    public function getRetryInterval(): int {
+        return $this->retry_interval;
+    }
+
+    public function withRetryInterval(int $retry_interval): self {
+        $this->retry_interval = $retry_interval;
+        return $this;
+    }
+
+    public function getReadTimeout(): float {
+        return $this->read_timeout;
+    }
+
+    public function withReadTimeout(float $read_timeout): self {
+        $this->read_timeout = $read_timeout;
+        return $this;
+    }
+
+    public function getAuth(): string {
+        return $this->auth;
+    }
+
+    public function withAuth(string $auth): self {
+        $this->auth = $auth;
+        return $this;
+    }
+
+    public function getDbIndex(): int {
+        return $this->dbIndex;
+    }
+
+    public function withDbIndex(int $dbIndex): self {
+        $this->dbIndex = $dbIndex;
+        return $this;
+    }
+
+    public static function getAvailableDrivers() {
+        return [
+            self::DRIVER_MYSQL,
+            self::DRIVER_PGSQL,
+            self::DRIVER_REDIS
+        ];
+    }
+
+}
