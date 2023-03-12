1b9ba1b1e5cd42bbaef81f59b141f9d91fbbe028
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-13 01:00:24 +0200

    Update postgres.php

--- examples/postgres.php
+++ examples/postgres.php
@@ -2,23 +2,20 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-Swoole\Runtime::enableCoroutine();
-
 $dbConfig = (new Db\UniversalConfig)
         ->withDriver('pgsql')
         ->withHost('127.0.0.1')
         ->withPort(5432)
         ->withDbName('test')
         ->withUsername('admin')
-        ->withPassword('admin');
+        ->withPassword('admin')
+        ->toArray();
 
 $dbPool = new Db\UniversalPool($dbConfig, 2);
 
-go(function () use (&$dbPool) {
-    $conn = $dbPool->get();
-    $result = $conn->query("select * from test;");
-    if ($result) {
-        var_dump($conn->fetchAll());
-    }
-    $dbPool->put($conn);
-});
+$conn = $dbPool->get();
+$result = $conn->query("select * from test;");
+if ($result) {
+    var_dump($conn->fetchAll());
+}
+$dbPool->put($conn);
