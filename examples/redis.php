305b70a567c845ba19edd0fd472a20a060a6c687
Pinga <121483313+getpinga@users.noreply.github.com>
2023-03-13 01:00:47 +0200

    Update redis.php

--- examples/redis.php
+++ examples/redis.php
@@ -2,20 +2,17 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-Swoole\Runtime::enableCoroutine();
-
 $dbConfig = (new Db\UniversalConfig)
         ->withDriver('redis')
         ->withHost('127.0.0.1')
         ->withPort(6379)
         ->withAuth('admin')
-        ->withReadTimeout(-1);
+        ->withReadTimeout(-1)
+        ->toArray();
 
 $dbPool = new Db\UniversalPool($dbConfig, 2);
 
-go(function () use (&$dbPool) {
-    $conn = $dbPool->get();
-    $conn->set('test', 'test_value');
-    var_dump($conn->get('test'));
-    $dbPool->put($conn);
-});
+$conn = $dbPool->get();
+$conn->set('test', 'test_value');
+var_dump($conn->get('test'));
+$dbPool->put($conn);
